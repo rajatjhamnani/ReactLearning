d0b8bca301b6f2185ee21ea0774b7a09a1b9a979
rajatjhamnani <rajatjhamnani@gmail.com>
2023-11-22 23:50:07 +0530

    PlaceHolder - Adding a filter which doesnt work on the expense tracker

--- src/component/Expenses/Expenses.js
+++ src/component/Expenses/Expenses.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React ,{useState} from 'react';
 import ExpenseItem from "./expenseitems"
 import './Expenses.css'
 import Card from '../UI/Card'
+import ExpensesFilter from './ExpensesFilter'
 const Expenses=(props)=>{
   //const {setExpenses}=props
     let arr=props.expenses
@@ -9,11 +10,13 @@ const Expenses=(props)=>{
     const onDelete=(id)=>{
       set(arr.filter((expense)=>expense.id!==id))
      }
+   
     return(
         <Card className="expenses">
+          <ExpensesFilter />
              {
         arr.map((expense,i)=>(
-          <ExpenseItem title={arr[i].title} amount={arr[i].amount} id={arr[i].id} date={arr[i].date} location={arr[i].location}
+        <ExpenseItem title={arr[i].title} amount={arr[i].amount} id={arr[i].id} date={arr[i].date} location={arr[i].location}
           onDelete={()=>onDelete(arr[i].id)}></ExpenseItem>
           
         ))
