cc50faa13b487ded22ab2eefa2da49f47ce2b650
rajatjhamnani <rajatjhamnani@gmail.com>
2023-11-22 09:49:38 +0530

    Submitting the forms

--- src/component/New Expense/ExpenseForm.js
+++ src/component/New Expense/ExpenseForm.js
@@ -4,20 +4,49 @@ function ExpenseForm(){
     const [enteredTitle , setEnteredTitle]=useState('')
     const [enteredAmount , setEnteredAmount]=useState('')
     const [enteredDate ,setEnteredDate]=useState('')
-    
+// const [userInput ,setuserInput]=useState({
+//     EnteredTitle:'',
+//     EnteredAmount:'',
+//     setEnteredDate:''
+
+// })
     const titlechange =(event)=>{
         setEnteredTitle(event.target.value)
+        // setuserInput({
+        //     ...userInput,
+        //     EnteredTitle:event.target.value
+        // })
+        // setuserInput((previousstate)=>{
+        //     return{...previousstate , EnteredTitle:event.target.value}
+        // })
     }
    
     const amountChange = (event) =>{
         setEnteredAmount(event.target.value)
+    //     setuserInput((previousstate)=>{
+    //         return{...previousstate , EnteredAmount:event.target.value}
+    //     })
     }
    
     const dateChange =(event)=>{
         setEnteredDate(event.target.value)
+    //    setuserInput((previousstate)=>{
+    //     return{...previousstate , EnteredDate:event.target.value}
+    // })
     }
+    const submitHandler =(event)=>{
+        event.preventDefault()
+       let enteredData={
+           title: enteredTitle,
+           amount: enteredAmount,
+           date:new Date(enteredDate)
+
+        }
+        console.log(enteredData)
+    }
+    
     return(
-        <form>
+        <form onSubmit={submitHandler}>
         <div className="new-expense__controls">
             <div className="new-expense__control">
                 <label>Title</label>
@@ -33,7 +62,7 @@ function ExpenseForm(){
             </div>
         </div>
         <div className="new-expense__actions">
-          <button type='submit'>Add Expense</button>
+          <button type='submit' >Add Expense</button>
         </div>
         </form>
       
